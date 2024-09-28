39c56b09d35ca08537317c988b96948f17659b8f
NailikGit <kilian.smeets@web.de>
2024-09-28 20:28:01 +0200

    changed struct & began print and init functions

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,20 @@ const std = @import("std");
 
 const Character = struct {
     const Self = @This();
-    var name = "";
-    var species = "";
+    const out = std.io.getStdOut().writer();
+    const in = std.io.getStdIn().reader();
+    const buf_size = 4096;
+    var input = [_]u8{0} ** buf_size;
+    var buf = std.io.fixedBufferStream(&input);
+    const writer = buf.writer();
+    const body_skill = .{ "Finesse", "Athletics", "Acrobatics", "Stealth", "Fine Motor Skills", "Music" };
+    const wit_skill = .{ "Persuasion", "Perception", "Leadership", "Improvisation", "Survival" };
+    const mind_skill = .{ "History", "Magical Knowledge", "Insight", "Alchemy", "Enchanting" };
+    const martial_arts = .{ "Unarmed", "Trauma and Cleaving", "Sword and Hilt Weapons", "Spears and Polearms", "Bows", "Crossbows", "Throwing Weapons" };
+    const magic = .{ "Ritual Magic", "Catalytic Magic", "Freeform Magic" };
+    name: []const u8,
+    species: []const u8,
+    pst: []const u8,
     body: u8,
     wit: u8,
     mind: u8,
@@ -13,34 +25,58 @@ const Character = struct {
     max_mvmt: u8,
     eff_mvmt: u8,
     currency: u32,
-    var armor_and_garments = std.SinglyLinkedList(*const u8);
-    var weapons = std.SinglyLinkedList(*const u8);
-    var tmp_effects = std.SinglyLinkedList(*const u8);
-    var perm_effects = std.SinglyLinkedList(*const u8);
-    var relics = std.SinglyLinkedList(*const u8);
-    var misc_items = std.SinglyLinkedList(*const u8);
-    var feats = std.SinglyLinkedList(*const u8);
-    var potions_and_bombs = std.SinglyLinkedList(*const u8);
+    body_skill_mastery: [6]u8,
+    wit_skill_mastery: [5]u8,
+    mind_skill_mastery: [5]u8,
+    martial_arts_mastery: [7]u8,
+    magic_mastery: [3]u8,
 
-    fn init(self: Self, body: u8, wit: u8, mind: u8) void {
-        self.body = body;
-        self.wit = wit;
-        self.mid = mind;
-        self.max_hp = body;
-        self.hp = body;
-        self.max_mvmt = body;
-        self.eff_mvmt = @floor(body / 2);
+    fn createCharacter(name: []const u8, species: []const u8, pst: []const u8, body: u8, wit: u8, mind: u8) Character {
+        return Character{
+            .name = name,
+            .species = species,
+            .pst = pst,
+            .body = body,
+            .wit = wit,
+            .mind = mind,
+            .endurance = 0,
+            .max_hp = body,
+            .hp = body,
+            .max_mvmt = body,
+            .eff_mvmt = @divFloor(body, 2),
+            .currency = 0,
+            .body_skill_mastery = .{ 0, 0, 0, 0, 0, 0 },
+            .wit_skill_mastery = .{ 0, 0, 0, 0, 0 },
+            .mind_skill_mastery = .{ 0, 0, 0, 0, 0 },
+            .martial_arts_mastery = .{ 0, 0, 0, 0, 0, 0, 0 },
+            .magic_mastery = .{ 0, 0, 0 },
+        };
+    }
+    fn loadCharacter() Character {
+        return Character; // TODO: do some json magic or smthn
+    }
+    fn print(self: Self) !void {
+        try out.print("{s}\n", .{self.name});
+        try in.streamUntilDelimiter(writer, '\n', buf_size);
+        const end: usize = getEnd();
+        try out.print("{s}\n", .{input[0..end]});
+    }
+    fn getEnd() usize {
+        var end: usize = buf_size;
+        for (0..buf_size) |i| {
+            if (input[i] == 0) {
+                end = i;
+                break;
+            }
+        }
+        return end;
+    }
+    fn takeDamage(self: Self, dmg: u8) void {
+        self.hp -= dmg;
     }
 };
 
 pub fn main() !void {
-    var c = Character;
-    c.init();
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("hi", .{});
-
-    try bw.flush();
+    var c = Character.createCharacter("sinep", "corps", "", 5, 2, 11);
+    try c.print();
 }
